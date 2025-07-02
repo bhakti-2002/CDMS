2e281e34013ce197d50c26c254d920600804355b
ankita-210 <promaxysankita@gmail.com>
2025-07-02 16:08:34 +0530

    Update register.jsx

--- register.jsx
+++ register.jsx
@@ -8,12 +8,12 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-blue-100 px-4">
       <div className="flex flex-col md:flex-row bg-white rounded-xl shadow-lg overflow-hidden max-w-4xl w-full">
         
-        {/* Left Image Panel */}
+        
         <div className="md:w-1/2 flex items-center justify-center bg-white p-4 ">
           <img src={cowImg} alt="Cow" className="w-3/4 h-auto rounded-md" />
         </div>
 
-        {/* Right Form Panel */}
+        
         <div className="md:w-1/2 p-8">
           <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">User Registration</h2>
 
@@ -36,7 +36,7 @@ function App() {
             </button>
           </form>
 
-          {/* Login Link */}
+          
           <p className="text-center text-sm mt-4">
             Have an Account?{' '}
             <a href="#" className="text-black font-semibold">
@@ -44,13 +44,13 @@ function App() {
             </a>
           </p>
 
-          {/* Google Button */}
+          
           <button className="mt-4 w-full flex items-center justify-center bg-pink-200 text-black py-2 rounded-md shadow-sm hover:bg-pink-300">
             <img src="https://img.icons8.com/color/24/000000/google-logo.png" alt="Google" className="mr-2" />
             Continue with Google
           </button>
 
-          {/* Back Link */}
+          
           <p className="text-center text-sm mt-4">
             <a href="#" className="text-black underline">Back to Home Page</a>
           </p>
